bac48b96f7e3ba5e470cb7d7ae84e9122d0ce9e9
iron-man-22 <gangadharrock220@gmail.com>
2025-09-07 14:55:38 +0530

    gangadhar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { Navbar } from './components/navbar/navbar';
+import { AuthService } from './services/auth.service';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, CommonModule],
   styleUrls: ['./app.css'],
 })
-export class App {}
+export class App {
+  isLoginPage = false;
+
+  constructor(
+    public authService: AuthService,
+    private router: Router
+  ) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      this.isLoginPage = event.url === '/login';
+    });
+  }
+
+  shouldShowNavbar(): boolean {
+    return this.authService.isLoggedIn() && !this.isLoginPage;
+  }
+}
